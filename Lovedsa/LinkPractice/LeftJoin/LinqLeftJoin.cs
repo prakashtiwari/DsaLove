e66f6c1e90e1af209182c30966efe3ec63c2df38
Prakash Tiwari <prakashtiwari.net@gmail.com>
2024-03-17 22:30:10 +0530

    Linq

---
+++ Lovedsa/LinkPractice/LeftJoin/LinqLeftJoin.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LinkPractice.LeftJoin
+{
+    public class LinqLeftJoin
+    {
+
+        public List<StudentDetail> GetStudentDetail()
+        {
+
+            List<Student> students = new List<Student>();
+            List<Dept> depts = new List<Dept>();
+
+            depts.Add(new Dept { Id = 1, Name = "Science" });
+
+            students.Add(new Student() { Name = "Prakash", Id = 1, DeptId = 1 });
+            students.Add(new Student() { Name = "Ishan", DeptId = 1, Id = 2 });
+            students.Add(new Student() { Name = "Beti", Id = 2 });
+
+            var res = (from s in students
+                      join de in depts on s.DeptId equals de.Id into temp
+                      from d in temp.DefaultIfEmpty()
+                      select new StudentDetail
+                      {
+                          DeptName=d== null?string.Empty:d.Name,
+                          Id=s.Id,
+                          Name=s.Name
+                      }).ToList();
+            return res;
+        }
+
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int DeptId { get; set; }
+    }
+
+    public class Dept
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class StudentDetail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DeptName { get; set; }
+    }
+}
---
+++ Lovedsa/LinkPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿using LinkPractice.LeftJoin;
+using System;
+
+namespace LinkPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            LinqLeftJoin lj = new LinqLeftJoin();
+            var result = lj.GetStudentDetail();
+            foreach (var i in result)
+            {
+                Console.WriteLine($"Name is {i.Name} and Dept is: {i.DeptName}");
+            }
+        }
+    }
+}
